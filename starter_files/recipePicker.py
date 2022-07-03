346e2dab6d1af2ba13697178123e6a0de33324ad
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-03 22:32:26 +0300

    fonts for Win

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -1,11 +1,17 @@
-from multiprocessing import connection
 import tkinter as tk
-from turtle import bgcolor
 from PIL import ImageTk
 import sqlite3
 from numpy import random
+import pyglet
 
 bg_color = "#3d6466"
+
+pyglet.font.add_file("fonts/Shanti-Regular.ttf")
+pyglet.font.add_file("fonts/Ubuntu-Bold.ttf")
+
+def clear_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 def fetch_db():
     connection = sqlite3.connect("data/recipes.db")
@@ -39,10 +45,11 @@ def pre_process(table_name, table_records):
 
 
 def load_frame1():
+    clear_widgets(frame2)
     frame1.tkraise()
     frame1.pack_propagate(False)
     # logo widget
-    logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo.png")
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
     logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
     logo_widget.image = logo_img
     logo_widget.pack()
@@ -52,14 +59,14 @@ def load_frame1():
             text="ready for your random recipe?",
             bg=bg_color,
             fg="white",
-            font=("TkMenuFont", 14)
+            font=("Shanti", 14)
             ).pack()
 
     # button widget
     tk.Button(
             frame1,
             text="SHUFFLE RECIPE",
-            font=("TkHeadingFont", 20),
+            font=("Ubuntu", 20),
             bg="#28392a",
             fg="white",
             cursor="hand2",
@@ -69,12 +76,13 @@ def load_frame1():
             ).pack(pady=20)
 
 def load_frame2():
+    clear_widgets(frame1)    
     frame2.tkraise()
     table_name, table_records = fetch_db()
     title, ingredients = pre_process(table_name, table_records)
 
     # logo widget
-    logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo_bottom.png")
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo_bottom.png")
     logo_widget = tk.Label(frame2, image=logo_img, bg=bg_color)
     logo_widget.image = logo_img
     logo_widget.pack(pady=20)
@@ -83,20 +91,20 @@ def load_frame2():
         text=title,
         bg=bg_color,
         fg="white",
-        font=("TkHeadingFont", 20)
+        font=("Ubuntu", 20)
         ).pack(pady=25)
 
     for i in ingredients:
         tk.Label(frame2,
                 text=i,
-                bg=bg_color,
+                bg="#28392a",
                 fg="white",
-                font=("TkMenuFont", 12)
-                ).pack()
+                font=("Shanti", 12)
+                ).pack(fill="both")
 
     tk.Button(frame2,
             text="BACK",
-            font=("TkHeadingFont", 18),
+            font=("Ubuntu", 18),
             bg="#28392a",
             fg="white",
             cursor="hand2",
@@ -114,7 +122,7 @@ root.eval("tk::PlaceWindow . center")
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
 frame2 = tk.Frame(root, bg=bg_color)
 for frame in (frame1, frame2):
-    frame.grid(row=0, column=0)
+    frame.grid(row=0, column=0, sticky="nesw")
 
 load_frame1()
 
