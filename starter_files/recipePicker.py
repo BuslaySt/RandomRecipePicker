3ff1699920ad3a3f980f6b4260f44155719c3b5b
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-03 21:36:03 +0300

    frames

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -4,43 +4,50 @@ from PIL import ImageTk
 
 bg_color = "#3d6466"
 
+def load_frame1():
+    frame1.pack_propagate(False)
+    # logo widget
+    logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo.png")
+    logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
+    logo_widget.image = logo_img
+    logo_widget.pack()
+
+    # instructions widget
+    tk.Label(frame1,
+            text="ready for your random recipe?",
+            bg=bg_color,
+            fg="white",
+            font=("TkMenuFont", 14)
+            ).pack()
+
+    # button widget
+    tk.Button(
+            frame1,
+            text="SHUFFLE",
+            font=("TkHeadingFont", 20),
+            bg="#28392a",
+            fg="white",
+            cursor="hand2",
+            activebackground="#badee2",
+            activeforeground="black",
+            command=lambda:load_frame2()
+            ).pack(pady=20)
+
 def load_frame2():
-    pass
+    print("Hello")
 
 # initialize app
 root = tk.Tk()
 root.title("Recipe Picker")
 root.eval("tk::PlaceWindow . center")
 
+# create a frame widget
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
-frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
+frame2 = tk.Frame(root, bg=bg_color)
+for frame in (frame1, frame2):
+    frame.grid(row=0, column=0)
 
-# frame1 widgets
-logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo.png")
-logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
-logo_widget.image = logo_img
-logo_widget.pack()
-
-tk.Label(frame1,
-         text="ready for your random recipe?",
-         bg=bg_color,
-         fg="white",
-         font=("TkMenuFont", 14)
-         ).pack()
-
-# button widget
-tk.Button(
-        frame1,
-        text="SHUFFLE",
-        font=("TkHeadingFont", 20),
-        bg="#28392a",
-        fg="white",
-        cursor="hand2",
-        activebackground="#badee2",
-        activeforeground="black",
-        command=load_frame2()
-        ).pack()
+load_frame1()
 
 # run app
 root.mainloop()
