8274a1c81d2650702aa331091350f537cf518068
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-03 11:20:20 +0300

    logo

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+from PIL import ImageTk
 
-# initiallize app
+# initialize app
 root = tk.Tk()
+root.title("Recipe Picker")
+root.eval("tk::PlaceWindow . center")
+
+frame1 = tk.Frame(root, width=500, height=600, bg="#3d6466")
+frame1.grid(row=0, column=0)
+
+# frame1 widgets
+ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
 
 # run app
 root.mainloop()
