d4f039d846ba97311df8e4c5d3470382b9123240
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-03 22:16:28 +0300

    frame2

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -23,11 +23,23 @@ def fetch_db():
     return table_name, table_records
 
 def pre_process(table_name, table_records):
+    # title
     title = table_name[:-6]
     title = "".join([char if char.islower() else " " + char for char in title])
-    print(title)
+
+    # ingredients
+    ingredients = []
+    for i in table_records:
+        name = i[1]
+        qty = i[2]
+        unit = i[3]
+        ingredients.append(qty + " " + unit + " of " + name)
+
+    return title, ingredients
+
 
 def load_frame1():
+    frame1.tkraise()
     frame1.pack_propagate(False)
     # logo widget
     logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo.png")
@@ -57,8 +69,41 @@ def load_frame1():
             ).pack(pady=20)
 
 def load_frame2():
+    frame2.tkraise()
     table_name, table_records = fetch_db()
-    pre_process(table_name, table_records)
+    title, ingredients = pre_process(table_name, table_records)
+
+    # logo widget
+    logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo_bottom.png")
+    logo_widget = tk.Label(frame2, image=logo_img, bg=bg_color)
+    logo_widget.image = logo_img
+    logo_widget.pack(pady=20)
+
+    tk.Label(frame2,
+        text=title,
+        bg=bg_color,
+        fg="white",
+        font=("TkHeadingFont", 20)
+        ).pack(pady=25)
+
+    for i in ingredients:
+        tk.Label(frame2,
+                text=i,
+                bg=bg_color,
+                fg="white",
+                font=("TkMenuFont", 12)
+                ).pack()
+
+    tk.Button(frame2,
+            text="BACK",
+            font=("TkHeadingFont", 18),
+            bg="#28392a",
+            fg="white",
+            cursor="hand2",
+            activebackground="#badee2",
+            activeforeground="black",
+            command=lambda:load_frame1()
+            ).pack(pady=20)
 
 # initialize app
 root = tk.Tk()
