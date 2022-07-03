e86ec7cadc2cfc122faf953874323dbf81421be4
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-07-03 11:51:36 +0300

    center logo and pack propagate false

--- starter_files/recipePicker.py
+++ starter_files/recipePicker.py
@@ -1,16 +1,23 @@
 import tkinter as tk
+from turtle import bgcolor
 from PIL import ImageTk
+
+bg_color = "#3d6466"
 
 # initialize app
 root = tk.Tk()
 root.title("Recipe Picker")
 root.eval("tk::PlaceWindow . center")
 
-frame1 = tk.Frame(root, width=500, height=600, bg="#3d6466")
+frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
 frame1.grid(row=0, column=0)
+frame1.pack_propagate(False)
 
 # frame1 widgets
-ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
+logo_img = ImageTk.PhotoImage(file="assets\RRecipe_logo.png")
+logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
+logo_widget.image = logo_img
+logo_widget.pack()
 
 # run app
 root.mainloop()
